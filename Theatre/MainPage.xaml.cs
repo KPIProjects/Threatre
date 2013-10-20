014d30d0435ba2258c8f74bf1583e4d7d3e5f337
prom4ik <iprom4ik@gmail.com>
2013-10-20 23:59:03 +0700

    Add source xamls

--- Theatre/MainPage.xaml.cs
+++ Theatre/MainPage.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
-namespace Theatre
+namespace PhoneApp1
 {
     public partial class MainPage : PhoneApplicationPage
     {
